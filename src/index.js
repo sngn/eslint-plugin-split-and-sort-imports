fb6476fdce3fdecdee90765d2ca5fdaff708b732
Maximilian Stejskal <max@sngn.net>
2025-04-20 11:03:08 +0200

    improve/fix types

--- src/index.js
+++ src/index.js
@@ -7,7 +7,24 @@ import rule_split from "./rules/split.js";
 import { URL } from "node:url";
 
 /** @typedef {import("eslint").Linter.Config} Config */
+/**
+ * `Plugin["meta"]` with all members present
+ * @typedef {object} MetaLocal
+ * @property {string} name
+ * @property {string} version
+ */
 /** @typedef {import("eslint").ESLint.Plugin} Plugin */
+
+/** @typedef {Record<"minimal"|"recommended", Config[]>} Configs */
+
+/**
+ * @typedef {object} IPluginLocal
+ * @property {Configs} configs
+ * @property {MetaLocal} meta
+ * @property {Exclude<Plugin["rules"], undefined>} rules
+ */
+
+/** @typedef {Plugin & IPluginLocal} PluginLocal */
 
 const pkg = JSON.parse(
   fs.readFileSync(new URL("../package.json", import.meta.url), "utf8"),
@@ -28,8 +45,8 @@ const plugin = {
   rules,
 };
 
-/** @type {Record<"minimal"|"recommended", Config[]>} */
-const configurations = {
+/** @type {Configs} */
+const configs = {
   minimal: [
     {
       plugins: {
@@ -54,6 +71,8 @@ const configurations = {
   ],
 };
 
-plugin.configs = configurations;
+plugin.configs = configs;
 
-export default plugin;
+const plugin_export = /** @type {PluginLocal} */ (plugin);
+
+export default plugin_export;
